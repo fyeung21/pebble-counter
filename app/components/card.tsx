b67632797da0810ccde2b7c32e2000ead7e15c83
Fiona Y. <fyeung21@gmail.com>
2025-11-09 23:54:06 -0800

    feat: setup counting click event

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Card() {
+  const [onClick, setonClick] = useState(0);
+
+  const handleClick = () => {
+    setonClick(onClick + 1);
+  };
   return (
     <article className="border-2 border-pink-400 w-60">
       <h3>{"toothpaste"}</h3>
-      <button className="border-2 border-blue-400">+ {"1"} button</button>
-      <p>clicked: {1}</p>
+      <button onClick={handleClick} className="border-2 border-blue-400">
+        + {"1"} button
+      </button>
+      <p>clicked: {onClick}</p>
       <p>date logged: {"Nov 9, 1:10am"}</p>
     </article>
   );
