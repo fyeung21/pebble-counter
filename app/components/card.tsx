534e3d928b6a06bb510d1f1f577befbfec880932
Fiona Y. <fyeung21@gmail.com>
2025-11-09 23:44:50 -0800

    feat: sketch card component

---
+++ app/components/card.tsx
@@ -0,0 +1,10 @@
+export default async function Card() {
+  return (
+    <article className="border-2 border-pink-400 w-60">
+      <h3>{"toothpaste"}</h3>
+      <button className="border-2 border-blue-400">+ {"1"} button</button>
+      <p>clicked: {1}</p>
+      <p>date logged: {"Nov 9, 1:10am"}</p>
+    </article>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Card from "./components/card";
 
 export default function Home() {
   return (
     <main>
-      <h2>content here</h2>
+      <Card />
     </main>
   );
 }
